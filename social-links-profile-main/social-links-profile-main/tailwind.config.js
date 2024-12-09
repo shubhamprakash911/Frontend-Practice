7290ba70e3bda760bfbcb546c5705f8e2764b5bd
Shubham <shubhamprakash9112000@gmail.com>
2024-12-09 13:44:17 +0530

    social links profile css practice

---
+++ social-links-profile-main/social-links-profile-main/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Figtree", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
