32c8cc5cc4df7e7cd730a8bc2752285d37124b3c
Shubham <shubhamprakash9112000@gmail.com>
2024-12-11 17:07:41 +0530

    css practice

---
+++ product-preview-card-component-main/product-preview-card-component-main/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+        Fraunces: ["Fraunces", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
